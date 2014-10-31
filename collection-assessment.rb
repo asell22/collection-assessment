e88cd3e860832a0ce32ad6a3b6162b89bd1ac462
asell22 <alexanderjsell@gmail.com>
2014-10-31 13:46:09 -0700

    Create collection-assessment.rb

---
+++ collection-assessment.rb
@@ -0,0 +1,12 @@
+STUDENTS = [
+{"First Name" => "John", "Last Name" => "Foley", "Email" => "john@gschool.it", "Class" => "Beginning snark"},
+{"First Name" => "Sylwester", "Last Name" => "Kelsey", "Email" => "sellie@gmail.com", "Class" => "Ruby Immersive"},
+{"First Name" => "Timothy", "Last Name" => "Rama", "Email" => "tim.rama@gmail.com", "Class" => "Ruby Immersive" },
+{"First Name" => "Kane", "Last Name" => "Baccigalupi", "Email" => "kane@gschool.it", "Class" => "C for dummies" },
+{"First Name" => "Nikita", "Last Name" => "Theodosius", "Email" => "nikita.theo@gmail.com", "Class" => "Ruby Immersive"},
+{"First Name" => "Roddy", "Last Name" =>  "Eldred", "Email" => "roddy.el@gmail.com", "Class" => "Ruby Immersive"},
+{"First Name" => "Martha", "Last Name" => "Berner", "Email" => "martha@gschool.it", "Class" => "Time travel for beginners"},
+{"First Name" => "Kofi", "Last Name" => "Thomas", "Email" =>  "k.thomas@hotmail.com", "Class" => "Ruby Immersive"}
+]
+
+puts STUDENTS
